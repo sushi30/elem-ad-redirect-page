b1b2c7b21c7930365cf89557c9d355b109959bb9
karins28 <karins1010@gmail.com>
2020-05-18 15:08:38 +0300

    Updates

---
+++ precache-manifest.aaf10ff12872a0c939e7eaf58eaae369.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08edcd7bc34ac06694524fb4ab6bec4c",
+    "url": "/elem-ad-redirect-page/index.html"
+  },
+  {
+    "revision": "c9352316050c717698ce",
+    "url": "/elem-ad-redirect-page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ee4666a0aa750c5da196",
+    "url": "/elem-ad-redirect-page/static/js/2.0306200d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/elem-ad-redirect-page/static/js/2.0306200d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9352316050c717698ce",
+    "url": "/elem-ad-redirect-page/static/js/main.4d626486.chunk.js"
+  },
+  {
+    "revision": "576c72431d355c815fe4",
+    "url": "/elem-ad-redirect-page/static/js/runtime-main.27381b45.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/elem-ad-redirect-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
