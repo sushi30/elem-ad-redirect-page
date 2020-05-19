08675adefe5546337471e17f0984253b1223b06c
imri <imri.paran@gmail.com>
2020-05-19 08:29:05 +0300

    some padding so messages are not aligned

--- src/Chat/Message.tsx
+++ src/Chat/Message.tsx
@@ -8,15 +8,16 @@ export default function Message({ sender, content, time }: any) {
         fontSize: 16,
         direction: "rtl",
         textAlign: "right",
-        marginBottom: 8,
+        marginBottom: 24,
         maxWidth: "100%",
+        [sender == Sender.WEBSITE ? "paddingLeft" : "paddingRight"]: 8,
       }}
     >
       <div style={{ width: 24 }} />
       <div
         style={{
           borderRadius: 8,
-          float: sender == Sender.VISITOR ? "left" : "right",
+          float: sender == Sender.WEBSITE ? "right" : "left",
           padding: 8,
           background: sender == Sender.WEBSITE ? "aqua" : "gainsboro",
         }}
